3d35e9253ba0cbbc2c0749209070f9e497bbd267
hisen <passer-by-sen@foxmail.com>
2023-08-28 14:41:59 +0800

    其他模块

---
+++ base-service/global/constant/comment.go
@@ -0,0 +1,16 @@
+package constant
+
+const (
+	CommentServiceAddr = "127.0.0.1:8882"
+	CommentServiceName = "CommentService"
+	UserServiceAddr    = "127.0.0.1:8887"
+	UserServiceName    = "UserService"
+	CommentListErrCode = 511
+	VideoNotExist      = "视频不存在"
+	VideoExistErrMsg   = "查找视频错误"
+
+	CommentActionErrCode     = 512
+	CommentCountErrCode      = 513
+	CommentServiceClientName = "CommentServiceClient"
+	UserServiceClientName    = "UserServiceClient"
+)
